41e9efa925266e5ef2359d32b7b87ad42dbf9149
Adolfo Garrido Garcia <agarridog@oesia.com>
2020-02-13 15:54:31 +0100

    recuperamos award

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM sirensolutions/siren-platform:latest
-
-RUN apt-get update && apt-get install -y python-pip git && pip install elasticsearch
-
-COPY script/init.sh /init.sh
--- script/carga-inicial.py
+++ script/carga-inicial.py
@@ -10,7 +10,11 @@ import ssl
 URL_ELASTICSEARCH = "https://localhost:9220"
 es_user = 'sirenadmin'
 es_pass = 'password'
-es_index_kg_api = 'tender'
+es_index_kg_api_tender = 'tender'
+es_index_kg_api_tender_document = 'tender-document'
+es_index_kg_api_award = 'award'
+es_index_kg_api_award_document = 'award-document'
+es_index_kg_api_award_supplier = 'award-supplier'
 es_index_search_api = 'search'
 list_of_id = []
 list_of_id_void = []
@@ -22,17 +26,20 @@ if str(os.environ.get('IDTENDER')) == "None":
 else:
 	IDTENDER = str(os.environ.get('IDTENDER'))
 
-if str(os.environ.get('TOTAL_DATOS_SEARCH')) == "None":
-	TOTAL_DATOS_SEARCH = '500'
+if str(os.environ.get('TOTAL_DATOS_TENDER_SEARCH')) == "None":
+	TOTAL_DATOS_TENDER_SEARCH = '100'
 else:
-	TOTAL_DATOS_SEARCH = str(os.environ.get('TOTAL_DATOS_SEARCH'))
+	TOTAL_DATOS_TENDER_SEARCH = str(os.environ.get('TOTAL_DATOS_TENDER_SEARCH'))
 
-if str(os.environ.get('TOTAL_DATOS')) == "None":
-	TOTAL_DATOS = '100'
+if str(os.environ.get('TOTAL_DATOS_TENDER')) == "None":
+	TOTAL_DATOS_TENDER = '100'
 else:
-	TOTAL_DATOS = str(os.environ.get('TOTAL_DATOS'))
+	TOTAL_DATOS_TENDER = str(os.environ.get('TOTAL_DATOS_TENDER'))
 
-
+if str(os.environ.get('TOTAL_DATOS_AWARD')) == "None":
+	TOTAL_DATOS_AWARD = '100'
+else:
+	TOTAL_DATOS_AWARD = str(os.environ.get('TOTAL_DATOS_AWARD'))
 
 
 # Habrimos el registro de trazas de los logs
@@ -40,8 +47,8 @@ f = open("salida.log", "w")
 f.write(" + Inicio del proceso de carga de datos" + "\n")
 f.write(" + Variables de Entorno:" + "\n")
 f.write("   - ID del Tender a consultar: " + IDTENDER + "\n")
-f.write("   - TOTAL_DATOS_SEARCH del numero de consultas a realizar en search-api: " + TOTAL_DATOS_SEARCH + "\n")
-f.write("   - TOTAL_DATOS para almacenar de tender en elasticsearch: " + TOTAL_DATOS + "\n")
+f.write("   - TOTAL_DATOS_TENDER_SEARCH del numero de consultas a realizar en search-api: " + TOTAL_DATOS_TENDER_SEARCH + "\n")
+f.write("   - TOTAL_DATOS_TENDER para almacenar de tender en elasticsearch: " + TOTAL_DATOS_TENDER + "\n")
 f.write("   - URL de ElasticSearch: " + URL_ELASTICSEARCH + "\n")
 f.write("     - Usuario: ElasticSearch: " + es_user + "\n")
 f.write("     - Password: ElasticSearch: " + es_pass + "\n")
@@ -58,44 +65,45 @@ while True:
 	print("---- Elasticsearch OK")
 	break
 
-# Borramos el indice si existe
-es.indices.delete(index=es_index_kg_api, ignore=[400, 404])
-es.indices.delete(index=es_index_search_api, ignore=[400, 404])
+# Borramos los datos del indice si existe
+es.indices.delete(index=es_index_search_api, ignore=404)
+es.indices.delete(index=es_index_kg_api_tender, ignore=404)
+es.indices.delete(index=es_index_kg_api_tender_document, ignore=404)
+es.indices.delete(index=es_index_kg_api_award, ignore=404)
+es.indices.delete(index=es_index_kg_api_award_document, ignore=404)
+es.indices.delete(index=es_index_kg_api_award_supplier, ignore=404)
 
 
-# Funcion que realiza un busqueda de Tender similares he inserta en elasticsearch los datos
-def iteracion (id_tender):
-	global TOTAL_DATOS
-	global TOTAL_DATOS_SEARCH
+
+###############################################
+###    SEARCH
+###############################################
+def iteracion_search (id_tender):
+	global TOTAL_DATOS_TENDER
+	global TOTAL_DATOS_TENDER_SEARCH
 	global LIMITE
 	if inserta_tender (id_tender):
-		f.write("+ Buscamos similares: " + "http://tbfy.librairy.linkeddata.es/search-api/documents/" + id_tender + "/items?source=tender&size=" + TOTAL_DATOS_SEARCH + "\n")
-		req = urllib2.Request("http://tbfy.librairy.linkeddata.es/search-api/documents/" + id_tender + "/items?source=tender&size=" + TOTAL_DATOS_SEARCH)
+		f.write("+ Buscamos similares: " + "http://tbfy.librairy.linkeddata.es/search-api/documents/" + id_tender + "/items?source=tender&size=" + TOTAL_DATOS_TENDER_SEARCH + "\n")
+		print("+ Buscamos similares: " + "http://tbfy.librairy.linkeddata.es/search-api/documents/" + id_tender + "/items?source=tender&size=" + TOTAL_DATOS_TENDER_SEARCH)
+		req = urllib2.Request("http://tbfy.librairy.linkeddata.es/search-api/documents/" + id_tender + "/items?source=tender&size=" + TOTAL_DATOS_TENDER_SEARCH)
 		response = urllib2.urlopen(req)
 		json_data_search_api = json.loads(response.read().decode('utf8', 'ignore'))
 		for rows in json_data_search_api:
-			insertar_indice_search (id_tender, rows['id'])
-			req_r = urllib2.Request("http://tbfy.librairy.linkeddata.es/search-api/documents/" + rows['id'] + "/items?source=tender&size=10")
-			response_r = urllib2.urlopen(req_r)
-			json_data_search_api_r = json.loads(response_r.read().decode('utf8', 'ignore'))
-			for rows_r in json_data_search_api_r:
-				insertar_indice_search (rows['id'], rows_r['id'])
-			if rows['id'] not in list_of_id and rows['id'] not in list_of_id_void and len(list_of_id) <= int(TOTAL_DATOS):
-				inserta_tender (rows['id'])
+			if rows['id'] not in list_of_id and rows['id'] not in list_of_id_void and len(list_of_id) <= int(TOTAL_DATOS_TENDER):
+				if inserta_tender (rows['id']):
+					insertar_indice_search (id_tender, rows['id'])
 	else:
 		print ("No se puede recuperar el Tender: " + id_tender + " por lo que no realizamos mas busquedas sobre este Tender")
-
-
 
 def insertar_indice_search (id_search, id_result):
 	doc = {
 		'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
 		'id_search': id_search,
-		'id_result': id_result
+		'id': id_result
 		}
 	while True:
 		try:
-			res = es.index(index=es_index_search_api, body=doc)
+			res = es.index(index=es_index_search_api, id=id_result, body=doc)
 		except:
 			print ("---- Fallo al insertar registro en \"" + es_index_search_api + "\" de ElasticSearch, esperamos 10 segundo")
 			print ("---- " + es_index_search_api + ": " + id_search + " -> " + id_result)
@@ -104,9 +112,138 @@ def insertar_indice_search (id_search, id_result):
 		break
 	print ("+ " + es_index_search_api + ": " + id_search + " -> " + id_result)
 	return True
-	
 
-# Funcion que consulta a KN-API un id y lo inserta en ElasticSearch
+###############################################
+###    AWARD
+###############################################
+def iteracion_award (total_award):
+	global TOTAL_DATOS_AWARD
+	f.write("+ Buscamos award: " + "http://tbfy.librairy.linkeddata.es/kg-api/award?size=" + total_award + "\n")
+	print("+ Buscamos similares: " + "http://tbfy.librairy.linkeddata.es/kg-api/award?size=" + total_award)
+	req = urllib2.Request("http://tbfy.librairy.linkeddata.es/kg-api/award?size=" + total_award)
+	response = urllib2.urlopen(req)
+	json_data_award_api = json.loads(response.read().decode('utf8', 'ignore'))
+	for rows in json_data_award_api:
+		inserta_award (rows['id'])
+
+def inserta_award (id):
+	req = urllib2.Request("http://tbfy.librairy.linkeddata.es/kg-api/award/" + id)
+	try:
+		response = urllib2.urlopen(req)
+	except:
+		print("Error en la llamada - " + "http://tbfy.librairy.linkeddata.es/kg-api/award/" + id)
+		print("")
+		time.sleep(2)
+		return False
+	json_data_kg_api = json.loads(response.read().decode('utf8', 'ignore'))
+	if json_data_kg_api.get('id'):
+		f.write ("  - Anadimos el id " + id + " award\n")
+	else:
+		f.write("  - No se ha recuperado datos de kg-api " + id + " award\n")
+		return False
+	doc = {
+		'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+		'id': id,
+		'title': json_data_kg_api.get('title'),
+		'description': json_data_kg_api.get('description'),
+		'date': json_data_kg_api.get('date'),
+		'contractPeriodStartDate': json_data_kg_api.get('contractPeriod').get("startDate"),
+		'contractPeriodEndDate': json_data_kg_api.get('contractPeriod').get("endDate"),
+		'awardValue': json_data_kg_api.get('value').get("amount"),
+		'awardCurrency': json_data_kg_api.get('value').get("currency"),
+		'tenderId': json_data_kg_api.get('tender').get("id"),
+		'status': json_data_kg_api.get('status')
+		}
+	while True:
+		try:
+			res = es.index(index=es_index_kg_api_award, id=id, body=doc)
+		except:
+			print ("---- Fallo al insertar registro en \"" + es_index_kg_api_award + "\" de ElasticSearch, esperamos 10 segundo")
+			print ("---- " + es_index_kg_api_award + ": " + id)
+			time.sleep(10)
+			continue
+		break
+	print ("+ " + es_index_kg_api_award + ": " + id)
+	inserta_award_document (id)
+	inserta_award_supplier (id)
+	return True
+
+
+def inserta_award_document (id):
+	req = urllib2.Request("http://tbfy.librairy.linkeddata.es/kg-api/award/" + id + "/document")
+	try:
+		response = urllib2.urlopen(req)
+	except:
+		print("Error en la llamada - " + "http://tbfy.librairy.linkeddata.es/kg-api/award/" + id + "/document\n")
+		print(response)
+		print("")
+		time.sleep(2)
+		return False
+	json_data_kg_api = json.loads(response.read().decode('utf8', 'ignore'))
+	for rows in json_data_kg_api:
+		doc = {
+			'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+			'id_award_source': id,
+			'id': rows.get('id'),
+			'type': rows.get('type'),
+			'language': rows.get('language'),
+			'url': rows.get('url')
+			}
+		while True:
+			try:
+				res = es.index(index=es_index_kg_api_award_document, id= rows['id'], body=doc)
+			except:
+				print ("---- Fallo al insertar registro en \"" + es_index_kg_api_award_document + "\" de ElasticSearch, esperamos 10 segundo")
+				print ("---- " + es_index_kg_api_award_document + ": " + id)
+				time.sleep(10)
+				continue
+			break
+		print ("+-- " + es_index_kg_api_award_document + ": " + id)
+	return True
+
+def inserta_award_supplier (id):
+	req = urllib2.Request("http://tbfy.librairy.linkeddata.es/kg-api/award/" + id + "/supplier")
+	try:
+		response = urllib2.urlopen(req)
+	except:
+		print("Error en la llamada - " + "http://tbfy.librairy.linkeddata.es/kg-api/award/" + id + "/supplier")
+		print("")
+		time.sleep(2)
+		return False
+	json_data_kg_api = json.loads(response.read().decode('utf8', 'ignore'))
+	for rows in json_data_kg_api:
+		doc = {
+			'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+			'id_award_source': id,
+			'id': rows.get('id'),
+			'legalname': rows.get('legalname'),
+			'jursidiction': rows.get('jursidiction'),
+			'street': rows.get('address').get('street'),
+			'postalCode': rows.get('address').get('postalCode'),
+			'locality': rows.get('address').get('locality'),
+			'country': rows.get('address').get('country'),
+			'contactName': rows.get('contactPoint').get('name'),
+			'contactEmail': rows.get('contactPoint').get('email'),
+			'contactTelephone': rows.get('contactPoint').get('telephone'),
+			'contactTelephone': rows.get('contactPoint').get('telephone'),
+			'contactURL': rows.get('contactPoint').get('URL'),
+			'contactFax': rows.get('contactPoint').get('fax')
+			}
+		while True:
+			try:
+				res = es.index(index=es_index_kg_api_award_supplier, id=id, body=doc)
+			except:
+				print ("---- Fallo al insertar registro en \"" + es_index_kg_api_award_supplier + "\" de ElasticSearch, esperamos 10 segundo")
+				print ("---- " + es_index_kg_api_award_supplier + ": " + id)
+				time.sleep(10)
+				continue
+			break
+		print ("+-- " + es_index_kg_api_award_supplier + ": " + id)
+	return True
+	
+###############################################
+###    TENDER
+###############################################
 def inserta_tender (id):
 	global list_of_id
 	global list_of_id_void
@@ -114,9 +251,8 @@ def inserta_tender (id):
 	try:
 		response = urllib2.urlopen(req)
 	except:
-		f.write("Error en la llamada - " + "http://tbfy.librairy.linkeddata.es/kg-api/tender/" + id + "\n")
-		f.write(response)
-		f.write("\n")
+		print("Error en la llamada - " + "http://tbfy.librairy.linkeddata.es/kg-api/tender/" + id)
+		print("")
 		time.sleep(2)
 		return False
 	json_data_kg_api = json.loads(response.read().decode('utf8', 'ignore'))
@@ -129,30 +265,75 @@ def inserta_tender (id):
 		return False
 	doc = {
 		'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
-		'id': json_data_kg_api['id'],
-		'title': json_data_kg_api['title'],
-		'description': json_data_kg_api['description'],
-		'status': json_data_kg_api['status']
+		'id': id,
+		'title': json_data_kg_api.get('title'),
+		'description': json_data_kg_api.get('description'),
+		'awardCriteria': json_data_kg_api.get('award').get('criteria'),
+		'awardCriteriaDetails': json_data_kg_api.get('award').get('criteriaDetails'),
+		'tenderPeriodStartDate': json_data_kg_api.get('tenderPeriod').get('StartDate'),
+		'tenderPeriodEndDate': json_data_kg_api.get('tenderPeriod').get('eEndDate'),
+		'minEstimatedValueAmount': json_data_kg_api.get('value').get('minEstimatedAmount'),
+		'minEstimatedValueCurrency': json_data_kg_api.get('value').get('minEstimatedCurrency'),
+		'maxEstimatedValueAmount': json_data_kg_api.get('value').get('maxEstimatedAmount'),
+		'maxEstimatedValueCurrency': json_data_kg_api.get('value').get('maxEstimatedCurrency'),
+		'eligibilityCriteria': json_data_kg_api.get('eligibilityCriteria'),
+		'status': json_data_kg_api.get('status')
 		}
 	while True:
 		try:
-			res = es.index(index=es_index_kg_api, body=doc)
+			res = es.index(index=es_index_kg_api_tender, id=id, body=doc)
 		except:
-			print ("---- Fallo al insertar registro en \"" + es_index_kg_api + "\" de ElasticSearch, esperamos 10 segundo")
-			print ("---- " + es_index_kg_api + ": " + id)
+			print ("---- Fallo al insertar registro en \"" + es_index_kg_api_tender + "\" de ElasticSearch, esperamos 10 segundo")
+			print ("---- " + es_index_kg_api_tender + ": " + id)
 			time.sleep(10)
 			continue
 		break
-	print ("+ " + es_index_kg_api + ": " + id + " - list_of_id: " + str(len(list_of_id)) + " - list_of_id_void: " + str(len(list_of_id_void)))
+	print ("+ " + es_index_kg_api_tender + ": " + id + " - list_of_id: " + str(len(list_of_id)) + " - list_of_id_void: " + str(len(list_of_id_void)))
+	inserta_tender_document (id)
 	return True
+
+def inserta_tender_document (id):
+	global list_of_id
+	global list_of_id_void
+	req = urllib2.Request("http://tbfy.librairy.linkeddata.es/kg-api/tender/" + id + "/document")
+	try:
+		response = urllib2.urlopen(req)
+	except:
+		print("Error en la llamada - " + "http://tbfy.librairy.linkeddata.es/kg-api/tender/" + id + "/document")
+		print("")
+		time.sleep(2)
+		return False
+	json_data_kg_api = json.loads(response.read().decode('utf8', 'ignore'))
+	for rows in json_data_kg_api:
+		doc = {
+			'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+			'id_tender_source': id,
+			'id': rows['id'],
+			'type': rows['type'],
+			'language': rows['language'],
+			'url': rows['url']
+			}
+		while True:
+			try:
+				res = es.index(index=es_index_kg_api_tender_document, id= rows['id'], body=doc)
+			except:
+				print ("---- Fallo al insertar registro en \"" + es_index_kg_api_tender_document + "\" de ElasticSearch, esperamos 10 segundo")
+				print ("---- " + es_index_kg_api_tender_document + ": " + id)
+				time.sleep(10)
+				continue
+			break
+		print ("+-- " + es_index_kg_api_tender_document + ": " + id)
+	return True
+
         
 
 ##################################
 # main
 ##################################
 
-iteracion(IDTENDER)
+iteracion_search(IDTENDER)
 
+iteracion_award (TOTAL_DATOS_AWARD)
 
 f.write("\n")
 f.write("Numero de elementos insertados: "+ str(len(list_of_id)) + "\n")
